08290948157fca88839bf8a210fb1168b7679670
LHPHUC2410 <lhphuc.2410@gmail.com>
2024-08-19 00:59:55 +0700

    add anotation

--- src/main/java/com/Estore/controller/CategoryController.java
+++ src/main/java/com/Estore/controller/CategoryController.java
@@ -6,16 +6,17 @@ import com.Estore.dto.request.CategoryRequest;
 import com.Estore.entity.Category;
 import com.Estore.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/category")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @PostMapping()
     public ApiResponse<Category> createCategory(@RequestBody CategoryRequest categoryRequest) {
         Category category = categoryService.create(categoryRequest);
         return ApiResponse.<Category>builder()
@@ -23,19 +24,22 @@ public class CategoryController {
                 .build();
     }
 
+    @GetMapping()
     public ApiResponse<List<CategoryResponse>> getAllCategories() {
         return ApiResponse.<List<CategoryResponse>>builder()
                 .result(categoryService.getAll())
                 .build();
     }
 
-    public ApiResponse<CategoryResponse> getCategoryById(String id) {
+    @GetMapping("/{id}")
+    public ApiResponse<CategoryResponse> getCategoryById(@PathVariable String id) {
         return ApiResponse.<CategoryResponse>builder()
                 .result(categoryService.getById(id))
                 .build();
     }
 
-    public ApiResponse<Void> deleteCategory(String id) {
+    @DeleteMapping("/{id}")
+    public ApiResponse<Void> deleteCategory(@PathVariable String id) {
         categoryService.delete(id);
         return ApiResponse.<Void>builder().build();
     }
