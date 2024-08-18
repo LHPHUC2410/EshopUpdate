c9fdfda654077ba20ef5cbbb1284ce9b5bc4c1e2
LHPHUC2410 <lhphuc.2410@gmail.com>
2024-08-19 01:28:50 +0700

    fix

--- src/main/java/com/Estore/entity/Category.java
+++ src/main/java/com/Estore/entity/Category.java
@@ -13,7 +13,7 @@ import java.util.Set;
 @Builder
 public class Category {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String name;
 }
